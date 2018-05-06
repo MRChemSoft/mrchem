9450a1c603636b4d3b3201f04bb90596ed1228ff
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-06 16:08:17 -0400

    Superbuilds, but must be improved

--- cmake/custom/main.cmake
+++ cmake/custom/main.cmake
@@ -5,8 +5,42 @@ configure_file (
     "${CMAKE_BINARY_DIR}/config.h"
     )
 
-set(CMAKE_INCLUDE_CURRENT_DIR ON)
-
 add_subdirectory(external)
-add_subdirectory(src)
+
+include(ExternalProject)
+ExternalProject_Add(${PROJECT_NAME}_core
+  DEPENDS
+    mrcpp_external
+    getkw_external
+    xcfun_external
+  SOURCE_DIR
+    ${PROJECT_SOURCE_DIR}/src
+  CMAKE_ARGS
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_STANDARD=11
+    -DCMAKE_CXX_EXTENSIONS=OFF
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON
+    # FIXME
+    -DEIGEN3_INCLUDE_DIR=${EIGEN3_INCLUDE_DIR}
+    -DMRCPP_DIR=${MRCPP_DIR}
+    -DXCFun_DIR=${XCFun_DIR}
+    # FIXME
+    -DXCFun_INCLUDEDIR=${XCFun_INCLUDEDIR}
+    # FIXME
+    -DXCFun_LIBRARIES=${XCFun_LIBRARIES}
+    -DGetkw_DIR=${Getkw_DIR}
+    # FIXME
+    -DGetkw_INCLUDEDIR=${Getkw_INCLUDEDIR}
+    # FIXME
+    -DGetkw_LIBRARIES=${Getkw_LIBRARIES}
+    -DPYTHON_SITE_INSTALL_DIR=${PYTHON_SITE_INSTALL_DIR}
+  CMAKE_CACHE_ARGS
+    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
+  BUILD_ALWAYS
+    1
+  INSTALL_COMMAND
+    ""
+  )
+
 add_subdirectory(pilot)
