0321f4c98900246dc95f32581387f8befce31e0f
Peter Wind <peter.wind@met.no>
2018-08-21 12:30:32 +0200

    OrbitalIterator for restricted number of orbitals at a time

--- src/qmfunctions/OrbitalIterator.cpp
+++ src/qmfunctions/OrbitalIterator.cpp
@@ -8,7 +8,24 @@ namespace mrchem {
 
 OrbitalIterator::OrbitalIterator(OrbitalVector &Phi)
         : iter(0),
+	  received_counter(0),
+	  sent_counter(0),
           orbitals(&Phi) {
+
+    //Find and save the orbitals each rank owns
+    int my_mpi_rank = mpi::orb_rank;
+    for (int impi = 0; impi < mpi::orb_size; impi++) {
+	std::vector<int> orb_ix;
+	for (int i = 0; i < this->orbitals->size(); i++) {
+	    Orbital &phi_i = (*this->orbitals)[i];
+	    int phi_rank = phi_i.rankID();
+	    if (phi_rank == impi) {
+		orb_ix.push_back(i);
+	    }
+	}
+	this->orb_ix_map.push_back(orb_ix);
+    }
+
 }
 
 // Receive all the orbitals, for non symmetric treatment
@@ -16,7 +33,10 @@ OrbitalIterator::OrbitalIterator(OrbitalVector &Phi)
 // Send own orbitals to half of the MPI, for symmetric treatment
 // The algorithm ensures that at each iteration, each MPI is communicating
 // with only one other MPI. This ensures good communication parallelism.
-bool OrbitalIterator::next( bool symmetric ) {
+// NB: iter counts the number of MPI looped over:
+//     there may be several calls to "next" for the same iter in case max_recv<0,
+//     or double iterations for symmetric treatment.
+bool OrbitalIterator::next( bool symmetric, int max_recv ) {
     mpi::free_foreign(*this->orbitals); //delete foreign orbitals
     //NB: for now we completely delete orbitals at each iteration. Could reuse the memory in next iteration.
     this->received_orbital_index.clear();
@@ -30,66 +50,131 @@ bool OrbitalIterator::next( bool symmetric ) {
 
     //  We communicate with one MPI per iteration:
     //   - Symmetric: do two iterations, send in one step receive in the other
-    //   - send ALL my orbitals to other_rank
-    //   - receive ALL orbitals owned by other_rank
+    //   - send ALL my orbitals to other_rank (or max_recv if set)
+    //   - receive ALL orbitals owned by other_rank (or max_recv if set)
     //   - other_rank increases by one for each iteration, modulo size
     // Each MPI pair will be treated exactly once at the end of the iteration process
 
     int my_rank = mpi::orb_rank;
     int max_rank = mpi::orb_size;
+    int already_received = 0; //counter for requested received orbitals jumped over this call
+    int already_sent = 0; //counter for requested sent orbitals jumped over this call
+    int received = 0; //number of new received orbitals this call
+    int sent = 0; //number of new sent orbitals this call
+    int maxget = max_recv; //max number of orbitals to receive
+    if (max_recv<0) maxget = this->orbitals->size(); //unlimited
 
     int nstep = 1;// send and receive in the same iteration
     if (symmetric) nstep = 2;// one iteration is receive, one iteration is send
 
     for (int step = 0; step < nstep; step++) {
 
-	if(this->iter >= mpi::orb_size) break;
+	bool IamReceiver = true;//default, both send and receive
+	bool IamSender   = true;//default, both send and receive
+
+	if (this->iter >= mpi::orb_size) break; //last iteration is completed
 
 	// Figure out which MPI to talk to
 	int other_rank = (max_rank + this->iter - my_rank)%max_rank;
 	//note: my_rank= (max_rank + this->iter - other_rank)%max_rank
-
+	int send_size = this->orb_ix_map[my_rank].size();
+	int recv_size = this->orb_ix_map[other_rank].size();
 	if (other_rank == my_rank) {
-	    // We send/receive to ourself
-	    for (int i = 0; i < this->orbitals->size(); i++) {
-		Orbital &phi_i = (*this->orbitals)[i];
-		if (mpi::my_orb(phi_i)) {
-		    this->received_orbital_index.push_back(i);
+	    // We receive from ourself
+	    IamSender   = false; //sending data is "for free"
+	    for (int i = 0; i < send_size and received < maxget; i++) {
+		int orb_ix = this->orb_ix_map[my_rank][i];
+		Orbital &phi_i = (*this->orbitals)[orb_ix];
+		already_received++;
+		if (already_received > this->received_counter) {
+		    this->received_orbital_index.push_back(orb_ix);
 		    this->received_orbitals.push_back(phi_i);
+		    this->received_counter++;
+		    received++;
 		}
 	    }
 	} else {
 
-	    bool IamReceiver = true;//default, both send and receive
-	    bool IamSender   = true;//default, both send and receive
-
-	    if(symmetric) {
+	    if (symmetric) {
 		// Either send or receive
 		// Figure out which one will send and receive
 		//(my_rank + other_rank )%2 flips each time other_rank changes by one
 		//(my_rank < other_rank) ensures that my_rank and other_rank are opposite
-		if ( (my_rank + other_rank + (my_rank < other_rank))%2 ) {
+		// my_rank = other_rank must correspond to a receive (since it implicitely means extra work later on)
+		if ((my_rank + other_rank + (my_rank < other_rank))%2) {
 		    IamReceiver = false;
 		} else {
 		    IamSender   = false;
 		}
 	    }
 
-	    for (int i = 0; i < this->orbitals->size(); i++) {
-		int tag = max_rank*this->iter + i;
-		Orbital &phi_i = (*this->orbitals)[i];
-		int phi_rank = phi_i.rankID();
-		if (phi_rank == other_rank and IamReceiver) {
-		    mpi::recv_orbital(phi_i, other_rank, tag);
-		    this->received_orbital_index.push_back(i);
-		    this->received_orbitals.push_back(phi_i);
+	    // highest mpi rank send first receive after, the other does the opposite
+	    if(my_rank > other_rank) {
+		//send
+		for (int i = 0; i < send_size and IamSender  and  sent < maxget; i++) {
+		    int tag = this->orbitals->size()*this->iter + i;
+		    int orb_ix = this->orb_ix_map[my_rank][i];
+		    Orbital &phi_i = (*this->orbitals)[orb_ix];
+		    already_sent++;
+		    if (already_sent > this->sent_counter) {
+			mpi::send_orbital(phi_i, other_rank, tag);
+			this->sent_counter++;//total to other_rank
+			sent++;//this call
+		    }
 		}
-		if (phi_rank == my_rank and IamSender) {
-		    mpi::send_orbital(phi_i, other_rank, tag);
+		//receive
+		for (int i = 0; i < recv_size and IamReceiver and received < maxget; i++) {
+		    int tag = this->orbitals->size()*this->iter + i;
+		    int orb_ix = this->orb_ix_map[other_rank][i];
+		    Orbital &phi_i = (*this->orbitals)[orb_ix];
+		    already_received++;
+		    if (already_received>this->received_counter) {
+			mpi::recv_orbital(phi_i, other_rank, tag);
+			this->received_orbital_index.push_back(orb_ix);
+			this->received_orbitals.push_back(phi_i);
+			this->received_counter++;//total from other_rank
+			received++;//this call
+		    }
 		}
+	    } else {
+		//my_rank < other_rank   -> receive first, send after
+		//receive
+		for (int i = 0; i < recv_size and IamReceiver and received < maxget; i++) {
+		    int tag = this->orbitals->size()*this->iter + i;
+		    int orb_ix = this->orb_ix_map[other_rank][i];
+		    Orbital &phi_i = (*this->orbitals)[orb_ix];
+		    already_received++;
+		    if (already_received>this->received_counter) {
+			mpi::recv_orbital(phi_i, other_rank, tag);
+			this->received_orbital_index.push_back(orb_ix);
+			this->received_orbitals.push_back(phi_i);
+			this->received_counter++;//total from other_rank
+			received++;//this call
+		    }
+		}
+		//send
+		for (int i = 0; i < send_size and IamSender and sent < maxget; i++) {
+		    int tag = this->orbitals->size()*this->iter + i;
+		    int orb_ix = this->orb_ix_map[my_rank][i];
+		    Orbital &phi_i = (*this->orbitals)[orb_ix];
+		    already_sent++;
+		    if (already_sent > this->sent_counter) {
+			mpi::send_orbital(phi_i, other_rank, tag);
+			this->sent_counter++;//total to other_rank
+			sent++;//this call
+		    }
+		}
+
 	    }
 	}
-	this->iter++;
+
+	if (    (this->sent_counter >= send_size or !IamSender)
+	    and (this->received_counter >= recv_size or !IamReceiver)) {
+	    //all orbitals to be processed during this iteration are ready. Start next iteration
+	    this->sent_counter = 0;
+	    this->received_counter = 0;
+	    this->iter++;
+	}
     }
     return true;
 }
--- src/qmfunctions/OrbitalIterator.h
+++ src/qmfunctions/OrbitalIterator.h
@@ -7,16 +7,19 @@ class OrbitalIterator final {
 public:
     OrbitalIterator(OrbitalVector &Phi);
 
-    bool next( bool symmetric = false );
+    bool next( bool symmetric = false , int max_recv = -1 );
     Orbital &get_orbital(int i) { return (this->received_orbitals)[i]; }
     int get_idx(int i) { return (this->received_orbital_index)[i]; }
     int get_size() { return this->received_orbitals.size(); }
 
 protected:
     int iter;
+    int received_counter; //number of orbitals fetched during this iteration
+    int sent_counter; //number of orbitals sent during this iteration
     OrbitalVector received_orbitals;
     std::vector<int> received_orbital_index;
     OrbitalVector *orbitals;
+    std::vector<std::vector<int>> orb_ix_map;//indices in the original orbital vector for each MPI
 };
 
 } //namespace mrchem
