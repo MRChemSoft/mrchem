51dfadd2c4c30d17fbe661d22e3ad4d3b27a9aaa
Gabriel Gerez <gabrielgerez@stallo-2.local>
2018-11-05 15:48:07 +0100

    add eval_epsilon function to Cavity

--- src/chemistry/Cavity.h
+++ src/chemistry/Cavity.h
@@ -31,15 +31,23 @@ using namespace mrcpp;
 
 namespace mrchem {
 
-class Cavity final : public mrcpp::RepresentableFunction<3> {
+
+class Cavity : public mrcpp::RepresentableFunction<3> {
 public:
-    Cavity(std::vector<mrcpp::Coord<3>> &coords, std::vector<double> &R, double slope);
-    double evalf(const mrcpp::Coord<3> &r) const override;
+  Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope);
+  Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope, double e_i, double e_o);
+  void eval_epsilon(bool argument, bool implement);
+  double evalf(const double *r) const override;
+  double evalf(const std::array<double, 3> &r) const {return evalf(r.data());};
 
 protected:
-    std::vector<mrcpp::Coord<3>> pos;
-    std::vector<double> R;
-    double d;
+  std::vector<std::array<double, 3>> pos;
+  std::vector<double> R;
+  double d;
+  double e_i = 1;
+  double e_o = 2;
+  bool b = false;
+  bool is_linear = false;
 };
 
 } // namespace mrchem
