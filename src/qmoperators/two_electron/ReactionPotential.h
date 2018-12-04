9afef0a18b8281319aed51410451bbf4d4916265
Gabriel Gerez <gabrielgerez@stallo-2.local>
2018-12-04 15:39:28 +0100

    add ReactionPotential to CMakelist.txt

--- src/qmoperators/two_electron/ReactionPotential.h
+++ src/qmoperators/two_electron/ReactionPotential.h
@@ -12,7 +12,7 @@ namespace mrchem {
 
 //start with a cavity initialized with a geometry and a standard gaussian rho with A*exp(-B*r^2) with A = (B/pi)^(3/2) include the orbitals later
 
-class ReactionPotential final : public QMPotential{
+class ReactionPotential : public QMPotential{
 public:
 
   ReactionPotential(mrcpp::PoissonOperator *P, mrcpp::DerivativeOperator<3> *D, Cavity *C, const Nuclei &nucs, OrbitalVector *Phi);
