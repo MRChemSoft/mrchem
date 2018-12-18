40c84e630ab00e6c187a41d041e489a4a2d72f24
Gabriel Gerez <gabrielgerez@stallo-2.local>
2018-12-18 12:54:23 +0100

    add ReactionOperator.h

---
+++ src/qmoperators/two_electron/ReactionOperator.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ReactionPotential.h"
+#include "ReactionPotentialD1.h"
+#include "ReactionPotentialD2.h"
+#include "qmoperators/RankZeroTensorOperator.h"
+
+/** @class ReactionOperator
+ *
+ * @brief Operator containing a single ReactionPotential
+ *
+ * This class is a simple TensorOperator realization of @class ReactionPotential.
+ *
+ */
+
+namespace mrchem {
+
+class ReactionOperator final : public RankZeroTensorOperator {
+public:
+    ReactionOperator(mrcpp::PoissonOperator *P, mrcpp::DerivativeOperator<3> *D, Cavity *C, const Nuclei &nuc, OrbitalVector *Phi)
+            : potential(new ReactionPotential(P, D, C, nuc, Phi)) {
+        RankZeroTensorOperator &J = (*this);
+        J = *this->potential;
+    }
+    
+    ~ReactionOperator() {
+        if (this->potential != nullptr) delete this->potential;
+    }
+
+
+    ComplexDouble trace(OrbitalVector &Phi) { return 0.5 * RankZeroTensorOperator::trace(Phi); }
+
+private:
+    ReactionPotential *potential;
+};
+
+} //namespace mrchem
