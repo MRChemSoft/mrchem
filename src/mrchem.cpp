0b6b2c3a3fb05a31f1ce40b852ac7f5e88baeb30
Stig Rune Jensen <stig.r.jensen@uit.no>
2020-04-07 16:10:16 +0200

    Add missing license headers

--- src/mrchem.cpp
+++ src/mrchem.cpp
@@ -1,15 +1,29 @@
-/** \mainpage The MRChem main program
+/*
+ * MRChem, a numerical real-space code for molecular electronic structure
+ * calculations within the self-consistent field (SCF) approximations of quantum
+ * chemistry (Hartree-Fock and Density Functional Theory).
+ * Copyright (C) 2020 Stig Rune Jensen, Luca Frediani, Peter Wind and contributors.
  *
- * \author Stig Rune Jensen
+ * This file is part of MRChem.
  *
- * \version 1.0
+ * MRChem is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * \par Copyright:
- * GPLv4
+ * MRChem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MRChem.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For information on the complete list of contributors to MRChem, see:
+ * <https://mrchem.readthedocs.io/>
  */
-#include "MRCPP/Timer"
-#include "parallel.h"
+
+#include <MRCPP/Timer>
 
 #include "driver.h"
 #include "mrchem.h"
