505bc78536f446f9a786eaed655be56ea1f05d54
Anders Brakestad <anders.brakestad@gmail.com>
2022-04-05 10:14:09 +0200

    Redesigned MRChemConstants. No longer inherits from qcelemental, but instantiates a PhysicalConstantsContext in __init__. This cleans up the class namespace a bit.

--- python/mrchem/physical_constants.py
+++ python/mrchem/physical_constants.py
@@ -29,10 +29,10 @@ from types import SimpleNamespace
 from qcelemental.physical_constants.context import PhysicalConstantsContext
 
 
-class MRChemPhysConstants(PhysicalConstantsContext):
-    """Wrapper over the PhysicalConstantsContext class from QCElemental.
-    Subclassing it here allows for some customization, and it ensures that
-    when imported the same CODATA source is used automatically (we use 2018).
+class MRChemPhysConstants:
+    """Class holding physical and math constants relevant to MRChem.
+    The constants are fetched from QCElemental, and new constants are
+    derived from those in QCElemental (as far as possible).
 
     Source in ascii:
     https://physics.nist.gov/cuu/Constants/Table/allascii.txt
@@ -43,26 +43,28 @@ class MRChemPhysConstants(PhysicalConstantsContext):
     PI = 3.1415926535897932384
     HARTREE2SIMAGNETIZABILITY = 78.9451185  # TODO: We should be able to derive this one
 
-    def __init__(self, context="CODATA2018"):
+    def __init__(self):
         """Here we extract those constants that we need to run any MRChem calculation.
         Those that are not directly available in QCElemental, we compute by via the existing
         constants in QCElementlal.
         """
-        super().__init__(context)
+        self.context = "CODATA2018"
+        self.nist = "https://physics.nist.gov/cuu/Constants/Table/allascii.txt"
+        qce = PhysicalConstantsContext(context=self.context)
 
         # Define new constants needed by MRChem
         # We follow the QCElemental 4-tuple format
         # (callname: str, units: str, value: float, description: str)
         mrchem_constants = [
-            ('pi',                              '',       self.PI,                                                                                            'Pi'),
-            ('pi_sqrt',                         '',       math.sqrt(self.PI),                                                                                 'Square root of pi'),
-            ('hartree2simagnetizability',       'J T^-2', self.HARTREE2SIMAGNETIZABILITY,                                                                     'Atomic units to J/T^2 (magnetizability)'),
-            ('atomic_unit_of_bohr_magneton',    '',       self.Bohr_magneton / self.atomic_unit_of_magnetizability / self.atomic_unit_of_mag_flux_density,    'Bohr magneton in atomic units'),
-            ('atomic_unit_of_nuclear_magneton', '',       self.nuclear_magneton / self.atomic_unit_of_magnetizability / self.atomic_unit_of_mag_flux_density, 'Nuclear magneton in atomic units'),
-            ('angstrom2bohrs',                  'Å',      1.0 / self.bohr2angstroms,                                                                          'Angstrom -> Bohr conversion factor')
+            ('pi',                              '',       self.PI,                                                                                         'Pi'),
+            ('pi_sqrt',                         '',       math.sqrt(self.PI),                                                                              'Square root of pi'),
+            ('hartree2simagnetizability',       'J T^-2', self.HARTREE2SIMAGNETIZABILITY,                                                                  'Atomic units to J/T^2 (magnetizability)'),
+            ('atomic_unit_of_bohr_magneton',    '',       qce.Bohr_magneton / qce.atomic_unit_of_magnetizability / qce.atomic_unit_of_mag_flux_density,    'Bohr magneton in atomic units'),
+            ('atomic_unit_of_nuclear_magneton', '',       qce.nuclear_magneton / qce.atomic_unit_of_magnetizability / qce.atomic_unit_of_mag_flux_density, 'Nuclear magneton in atomic units'),
+            ('angstrom2bohrs',                  'Å',      1.0 / qce.bohr2angstroms,                                                                        'Angstrom -> Bohr conversion factor')
         ]
 
-        # Add the following constants to our mrchem subset
+        # Add the following pre-defined constants to our mrchem subset
         # NOTE: when using the get method, we use the NIST access names
         # which may contain spaces. See web page for reference.
         names = [
@@ -78,20 +80,19 @@ class MRChemPhysConstants(PhysicalConstantsContext):
 
         # Append the data to our list of constants
         for name in names:
-            datum = self.get(name, return_tuple=True)
+            datum = qce.get(name, return_tuple=True)
             constant = (datum.label, datum.units, datum.data, datum.comment)
             mrchem_constants.append(constant)
 
-        # Store our constants in a SimpleNamespace for dotted access in Python
-        self.mrc = SimpleNamespace(**{})
+        # Set our constants to instance attributes
         for ident, _, value, _ in sorted(mrchem_constants, key=lambda x: x[0]):
-            key = ident.lower().translate(self._transtable)
-            self.mrc.__setattr__(key, float(value))
+            key = ident.lower().translate(qce._transtable)
+            self.__setattr__(key, float(value))
 
     def print_constants_for_tests(self, varname='testConstants'):
         """Helper function for printing constants for copy/pasting into the c++ code.
         We need to store the constants internally for the tests to pass."""
-        for key, value in self.mrc.__dict__.items():
+        for key, value in self.__dict__.items():
             print(f'{varname}["{key}"] = {value};')
 
 if __name__ == '__main__':
