9a688ba2a92b06eeba7b2555baaccc6148761262
Anders Brakestad <anders.brakestad@gmail.com>
2022-04-06 15:29:59 +0200

    Now fetch constants from user_dict

--- python/mrchem/api.py
+++ python/mrchem/api.py
@@ -64,7 +64,7 @@ def translate_input(user_dict):
         "molecule": mol_dict,
         "scf_calculation": scf_dict,
         "rsp_calculations": rsp_dict,
-        "constants": pc.__dict__
+        "constants": user_dict["Constants"]
     }
     return program_dict
 
