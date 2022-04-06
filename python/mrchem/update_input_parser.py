d0b5d1d1f22fa7bae61983754bb37f1247579eaa
Anders Brakestad <anders.brakestad@gmail.com>
2022-04-06 15:23:18 +0200

    Added description to CLI interface

--- python/mrchem/update_input_parser.py
+++ python/mrchem/update_input_parser.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 
+#
+# MRChem, a numerical real-space code for molecular electronic structure
+# calculations within the self-consistent field (SCF) approximations of quantum
+# chemistry (Hartree-Fock and Density Functional Theory).
+# Copyright (C) 2022 Stig Rune Jensen, Luca Frediani, Peter Wind and contributors.
+#
+# This file is part of MRChem.
+#
+# MRChem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MRChem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with MRChem.  If not, see <https://www.gnu.org/licenses/>.
+#
+# For information on the complete list of contributors to MRChem, see:
+# <https://mrchem.readthedocs.io/>
+#
+
+
 from pathlib import Path
 from ruamel.yaml import YAML
 import subprocess
@@ -61,7 +87,7 @@ def update_doc():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="CLI for synchronizing the template with current constants, plus updating the input parser and user ref.")
     parser.add_argument('-sT', '--skip-template', action='store_true', help="Do not update constants section in template.yml")
     parser.add_argument('-sP', '--skip-parselglossy', action='store_true', help='Do not update input parser with parselglossy')
     parser.add_argument('-sD', '--skip-doc', action='store_true', help='Do not update the user reference file')
