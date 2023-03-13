132e63c2f9426666045a7355282c20ad4697ed22
Gabriel Gerez <gabriel.a.gerez.s@uit.no>
2023-03-13 18:00:55 +0100

    debugging for two orb tda

--- src/scf_solver/ExcitedStatesSolver.cpp
+++ src/scf_solver/ExcitedStatesSolver.cpp
@@ -2,7 +2,7 @@
  * MRChem, a numerical real-space code for molecular electronic structure
  * calculations within the self-consistent field (SCF) approximations of quantum
  * chemistry (Hartree-Fock and Density Functional Theory).
- * Copyright (C) 2022 Stig Rune Jensen, Luca Frediani, Peter Wind and contributors.
+ * Copyright (C) 2023 Stig Rune Jensen, Luca Frediani, Peter Wind and contributors.
  *
  * This file is part of MRChem.
  *
@@ -69,7 +69,7 @@ namespace mrchem {
  */
 json ExcitedStatesSolver::optimize(Molecule &mol, FockBuilder &F_0, FockBuilder &F_1, int state) {
     // should start with a "computing frequency initial guess" , probably should do it in driver.cpp
-    
+
     Timer t_tot;
     json json_out;
     double err_o = 1.0;
@@ -114,7 +114,7 @@ json ExcitedStatesSolver::optimize(Molecule &mol, FockBuilder &F_0, FockBuilder 
 
     // compute initial omega
 
-    double omega_n = computeOmega(Phi_0, X_n, F_0, V_1, F_mat_0);
+    double omega_n = computeOmega(Phi_0, X_n, F_0, V_1, F_mat_0); // 0.14949144; // hardcoded excitation energy value, from the veloxchem calculation
     double domega_n = omega_n;
 
     this->property.push_back(omega_n);
@@ -193,6 +193,8 @@ json ExcitedStatesSolver::optimize(Molecule &mol, FockBuilder &F_0, FockBuilder 
             // Prepare for next iteration
             X_n = orbital::add(1.0, X_n, 1.0, dX_n);
             // orbital::orthogonalize(this->orth_prec, X_n);
+            orbital::orthogonalize(this->orth_prec, X_np1, Phi_0);
+            orbital::orthogonalize(this->orth_prec, X_np1);
 
             // Setup perturbed Fock operator (including V_1)
             V_1.clear();
@@ -202,8 +204,9 @@ json ExcitedStatesSolver::optimize(Molecule &mol, FockBuilder &F_0, FockBuilder 
             mrcpp::print::header(2, "Computing frequency update");
             t_lap.start();
 
-            auto omega_np1 = computeOmega(Phi_0, X_n, F_0, V_1, F_mat_0); /*  computeOmega(Phi_0, X_n, Y_n, F_0, F_1, F_mat_0); */
-            domega_n = omega_np1 - omega_n;                               // maybe I should do this before normalization
+            auto omega_np1 = computeOmega(Phi_0, X_n, F_0, V_1, F_mat_0);
+                /*  computeOmega(Phi_0, X_n, Y_n, F_0, F_1, F_mat_0); */ // 0.14949144; // hardcoded excitation energy value, from veloxchem calculation.
+            domega_n = omega_np1 - omega_n;                              // maybe I should do this before normalization
             omega_n += domega_n;
             mrcpp::print::footer(2, t_lap, 2);
             if (plevel == 1) mrcpp::print::time(1, "Computing frequency update", t_lap);
@@ -242,6 +245,7 @@ json ExcitedStatesSolver::optimize(Molecule &mol, FockBuilder &F_0, FockBuilder 
             mrcpp::print::header(2, "Computing frequency");
             t_lap.start();
             omega_n = computeOmega(Phi_0, X_n, F_0, V_1, F_mat_0); /*  computeOmega(Phi_0, X_n, Y_n, F_0, F_1, F_mat_0); */
+            std::cout << "last omega: " << omega_n << "\n";
             mrcpp::print::footer(2, t_lap, 2);
             if (plevel == 1) mrcpp::print::time(1, "Computing frequency", t_lap);
             this->property.push_back(omega_n);
@@ -273,12 +277,11 @@ double ExcitedStatesSolver::computeOmega(OrbitalVector &Phi, OrbitalVector &X, F
     OrbitalVector Psi = V_1(Phi);
 
     orbital::orthogonalize(this->orth_prec, Psi, Phi);
-
     auto xi_t_Q_Psi_vec = orbital::dot(X, Psi).sum().real();
 
     auto F_rr = xi_t_F_0_xi_vec + xi_t_Q_Psi_vec;
     auto X_dot_X = xi_t_xi_vec.sum().real();
-    auto omega = (F_rr - sum_ei_xi_t_xi) / X_dot_X;
+    auto omega = (F_rr - sum_ei_xi_t_xi) / (X_dot_X);
 
     return omega;
 }
