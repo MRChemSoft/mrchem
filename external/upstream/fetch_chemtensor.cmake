d32e9b9014f6a7022a788c2fd767544322169294
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2025-12-16 17:07:00 +0100

    Add missing chemtensor CMake files

---
+++ external/upstream/fetch_chemtensor.cmake
@@ -0,0 +1,23 @@
+# chemtensor doesn't support find_package yet
+message(STATUS "Downloading and building chemtensor.")
+include(FetchContent)
+FetchContent_Declare(chemtensor
+  QUIET
+	GIT_REPOSITORY https://github.com/qc-tum/chemtensor.git
+	GIT_TAG 8833dd854e55d348c069a952c8e548cce63bd9e1
+)
+
+# We save CMAKE_BUILD_TYPE, as we will set it to Release for externals
+set(_build_type ${CMAKE_BUILD_TYPE})
+set(_build_shared ${BUILD_SHARED_LIBS})
+set(CMAKE_BUILD_TYPE "Release")
+set(BUILD_SHARED_LIBS OFF)
+
+set(ENABLE_CHEMTENSOR_UNIT_TESTS OFF CACHE BOOL "" FORCE)
+set(ENABLE_CHEMTENSOR_PYTHON_MODULE OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(chemtensor)
+
+# Restore the settings
+set(CMAKE_BUILD_TYPE ${_build_type})
+set(BUILD_SHARED_LIBS ${_build_shared})
