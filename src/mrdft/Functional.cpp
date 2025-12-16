6a27e9a24feff623930168f893b6a2714a20bd2b
Susi Lehtola <susi.lehtola@gmail.com>
2025-12-16 16:11:49 +0200

    New look through the code

--- src/mrdft/Functional.cpp
+++ src/mrdft/Functional.cpp
@@ -146,7 +146,6 @@ Eigen::MatrixXd Functional::evaluate_transposed(Eigen::MatrixXd &inp) const {
                     exc = Eigen::VectorXd::Zero(nPts);
                     vxc = Eigen::VectorXd::Zero(nPts);
                     if (isSpin()) {
-                        // Susi : this is wrong
                         for (size_t k = 0; k < nPts; k++) {
                             rho_spin(k * 2, 0) = inp(k, 0);
                             rho_spin(k * 2 + 1, 0) = inp(k, 1);
@@ -173,20 +172,21 @@ Eigen::MatrixXd Functional::evaluate_transposed(Eigen::MatrixXd &inp) const {
                     break;
                 case XC_FAMILY_GGA:
                 case XC_FAMILY_HYB_GGA:
-                    exc = Eigen::MatrixXd::Zero(nPts, 1);
-                    vxc = Eigen::MatrixXd::Zero(nPts, 1);
-                    sxc = Eigen::MatrixXd::Zero(nPts, 1);
-                    sigma = Eigen::MatrixXd::Zero(nPts, 1);
                     if (isSpin()) {
-                        // Susi : this is wrong
+                        exc = Eigen::MatrixXd::Zero(nPts, 1);
+                        vxc = Eigen::MatrixXd::Zero(2 * nPts, 1);
+                        sxc = Eigen::MatrixXd::Zero(3 * nPts, 1);
+                        sigma = Eigen::MatrixXd::Zero(3 * nPts, 1);
                         for (size_t k = 0; k < nPts; k++) {
                             rho_spin(k * 2, 0) = inp(k, 0);
                             rho_spin(k * 2 + 1, 0) = inp(k, 1);
                         }
                         for (size_t j = 0; j < nPts; j++) {
-                            // Susi: this is wrong
-                            sigma(j) = inp(j, 1) * inp(j, 1) + inp(j, 2) * inp(j, 2) + inp(j, 3) * inp(j, 3) *
-                                       inp(j, 4) * inp(j, 4) + inp(j, 5) * inp(j, 5) + inp(j, 6) * inp(j, 6); 
+                            // Susi: check that the code is correct
+                            // Libxc expects reduced gradients: up-up, up-down, down-down
+                            sigma(3 * j, 0) = inp(j, 1) * inp(j, 1) + inp(j, 2) * inp(j, 2) + inp(j, 3) * inp(j, 3);
+                            sigma(3 * j + 1, 0) = inp(j, 1) * inp(j, 4) + inp(j, 2) * inp(j, 5) + inp(j, 3) * inp(j, 5);
+                            sigma(3 * j + 2, 0) = inp(j, 4) * inp(j, 4) + inp(j, 5) * inp(j, 5) + inp(j, 6) * inp(j, 6);
                         }
                         xc_gga_exc_vxc(&libxc_objects[i], nPts, rho_spin.col(0).data(), sigma.data(), exc.data(), vxc.data(), sxc.data());
 
@@ -198,6 +198,8 @@ Eigen::MatrixXd Functional::evaluate_transposed(Eigen::MatrixXd &inp) const {
                             out(j, 1) += exc(j, 1) * libxc_coeffs[i] * inp(j, 1);
                             out(j, 2) += vxc(j, 0) * libxc_coeffs[i];
                             out(j, 3) += vxc(j, 1) * libxc_coeffs[i];
+
+                            // Susi: this is wrong
                             out(j, 4) += 2 * sxc(j, 0) * inp(j, 1) * libxc_coeffs[i];
                             out(j, 5) += 2 * sxc(j, 1) * inp(j, 2) * libxc_coeffs[i];
                             out(j, 6) += 2 * sxc(j, 2) * inp(j, 3) * libxc_coeffs[i];
@@ -206,9 +208,11 @@ Eigen::MatrixXd Functional::evaluate_transposed(Eigen::MatrixXd &inp) const {
                             out(j, 9) += 2 * sxc(j, 2) * inp(j, 6) * libxc_coeffs[i];
                         }
                     } else {
-                        for (size_t j = 0; j < nPts; j++) { 
-                            sigma(j) = inp(j, 1) * inp(j, 1) + inp(j, 2) * inp(j, 2) + inp(j, 3) * inp(j, 3); 
-                        }
+                        exc = Eigen::MatrixXd::Zero(nPts, 1);
+                        vxc = Eigen::MatrixXd::Zero(nPts, 1);
+                        sxc = Eigen::MatrixXd::Zero(nPts, 1);
+                        sigma = Eigen::MatrixXd::Zero(nPts, 1);
+                        for (size_t j = 0; j < nPts; j++) { sigma(j) = inp(j, 1) * inp(j, 1) + inp(j, 2) * inp(j, 2) + inp(j, 3) * inp(j, 3); }
                         xc_gga_exc_vxc(&libxc_objects[i], nPts, inp.col(0).data(), sigma.data(), exc.data(), vxc.data(), sxc.data());
 
                         for (size_t j = 0; j < nPts; ++j) {
