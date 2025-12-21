54b013d97bc1157aff2a0c261a998524054e5018
Susi Lehtola <susi.lehtola@gmail.com>
2025-12-21 22:01:50 +0200

    Drop some hardcoding in the script

--- tools/sad.py
+++ tools/sad.py
@@ -5,15 +5,15 @@ import basis_set_exchange as bse
 from pyscf import gto, scf
 from pyscf.scf.atom_ks import get_atm_nrks
 
-def uncontract_dfunctions(basname='AHGBS-7', atomic_symbol='He'):
+def uncontract_dfunctions(basis_name, atomic_symbol):
     """Fetch AHGBS-7 from basis set exchange and uncontract all functions if contractions are present."""
     # Fetch basis set in NWChem format from BSE.
-    bas = bse.get_basis(name=basname, elements=[atomic_symbol], fmt='nwchem', header=True, uncontract_segmented=True)
+    bas = bse.get_basis(name=basis_name, elements=[atomic_symbol], fmt='nwchem', header=True, uncontract_segmented=True)
     
     return bas
 
-def get_basis_in_mrchem_format(atomic_symbol):
-    bas = bse.convert_formatted_basis_str(uncontract_dfunctions(atomic_symbol=atomic_symbol), 'nwchem', 'dalton')
+def get_basis_in_mrchem_format(basis_name, atomic_symbol):
+    bas = bse.convert_formatted_basis_str(uncontract_dfunctions(basis_name=basis_name, atomic_symbol=atomic_symbol), 'nwchem', 'dalton')
 
     Z = bse.lut.element_Z_from_sym(atomic_symbol)
     if Z > 20:
@@ -42,7 +42,7 @@ def get_basis_in_mrchem_format(atomic_symbol):
             new.append(' '.join(list(map(lambda x: f'{float(x):.12e}', line.split()))))
 
     b = []
-    b.append('Gaussian basis 3-21G\n')
+    b.append(f'Gaussian basis {basis_name}\n')
     b.append('1\n')
     b.append(f'{Z}. 1 {nshells} {nfuncs}\n')
     b.append(f'{atomic_symbol} {0:.12f} {0:.12f} {0:.12f}\n')
@@ -52,10 +52,10 @@ def get_basis_in_mrchem_format(atomic_symbol):
     return "".join(b)
 
 
-def sad(atomic_symbol):
+def sad(basis_name, atomic_symbol):
     """Compute density matrix for passed atom, and write density and basis set to file."""
     # Build molecule input
-    basis = uncontract_dfunctions(atomic_symbol=atomic_symbol, basname='AHGBS-7')
+    basis = uncontract_dfunctions(basis_name=basis_name, atomic_symbol=atomic_symbol)
     mol = gto.Mole(atom=f'{atomic_symbol} 0.0 0.0 0.0', basis=basis, symmetry=False, verbose=False)
     mol.spin = mol.tot_electrons() % 2
     mol.build()
@@ -72,7 +72,7 @@ def sad(atomic_symbol):
 
     # Flatten density matrix and get basis set in mrchem format
     D_fmt = [f'{x:.12e}' for _ in D.tolist() for x in _]
-    basis_mrc = get_basis_in_mrchem_format(atomic_symbol)
+    basis_mrc = get_basis_in_mrchem_format(basis_name, atomic_symbol)
 
     # Write files
     with open(atomic_symbol + '.dens', 'w') as d, open(atomic_symbol + '.bas', 'w') as b:
@@ -88,10 +88,11 @@ def sad(atomic_symbol):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate SAD guess files for MRChem')
     parser.add_argument('element', help='Atomic symbol for which you want to generate SAD files.')
+    parser.add_argument('--basis', help='Basis set you want to use for SAD generation.', default='AHGBS-7')
     args = parser.parse_args()
     
-    e = sad(args.element)
-    print(f'{"LDA/AHGBS-7 energy for":15}: {args.element}')
+    e = sad(args.basis, args.element)
+    print(f'{f"LDA/{args.basis} energy for":15}: {args.element}')
     print(f'{"Total energy":15}: {e:.8f} a.u.')
     print(f'{"Density file":15}: {args.element}.dens')
     print(f'{"Basis set file":15}: {args.element}.bas')
