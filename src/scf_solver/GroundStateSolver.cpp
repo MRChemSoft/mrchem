5be422602bc641f23684b5d9279f8a8210cd3006
edinvay <evgeni36@yandex.ru>
2026-01-05 20:28:17 +0100

    added calculation of grad_E

--- src/scf_solver/GroundStateSolver.cpp
+++ src/scf_solver/GroundStateSolver.cpp
@@ -270,8 +270,8 @@ json GroundStateSolver::optimize(Molecule &mol, FockBuilder &F) {
         printConvergenceRow(0);
     }
 
-    // Initialize Resolvent
-    HelmholtzVector Resolvent(getHelmholtzPrec(), Eigen::VectorXd::Constant(Phi_n.size(), -1.0));
+    // Initialize Resolvent (Attention: HelmholtzVector = -2 Helmholtz)
+    HelmholtzVector Minus_2_Resolvent(getHelmholtzPrec(), Eigen::VectorXd::Constant(Phi_n.size(), -1.0));
     
 
     int nIter = 0;
@@ -295,24 +295,34 @@ json GroundStateSolver::optimize(Molecule &mol, FockBuilder &F) {
 
         // Init Helmholtz operator
         HelmholtzVector H(helm_prec, F_mat.real().diagonal());
-        //println(0, "Helmholtz precision: " + std::to_string(helm_prec));
-        //println(0, "Orbital precision:   " + std::to_string(orb_prec));
-        //Eigen::VectorXd v = F_mat.real().diagonal();
-        //std::cout << "Helmholtz parameters: mu = " << v.transpose() << std::endl;
-        std::cout << "Fock matrix:" << std::endl << F_mat.real() << std::endl;
+        //std::cout << "Fock matrix:" << std::endl << F_mat.real() << std::endl;
         ComplexMatrix L_mat = H.getLambdaMatrix();
         
         // Apply Helmholtz operator
         OrbitalVector Psi = F.buildHelmholtzArgument(orb_prec, Phi_n, F_mat, L_mat);
-//        OrbitalVector Phi_np1 = H(Psi);
-        OrbitalVector grad_E = orbital::add(1.0, Phi_n, 1.0, Psi);
+        OrbitalVector Phi_np1 = H(Psi);
+        OrbitalVector grad_E = orbital::add(1.0, Phi_n, -2.0, Psi);
         Psi.clear();
-        grad_E = Resolvent(grad_E);
-        grad_E = orbital::add(2.0, Phi_n, -2.0, grad_E);
+        grad_E = Minus_2_Resolvent(grad_E);
+        grad_E = orbital::add(2.0, Phi_n, 1.0, grad_E);
 
+        OrbitalVector Minus_2_Resolvent_Phi = Minus_2_Resolvent(Phi_n);
+        ComplexMatrix B_proj = -0.5 * orbital::calc_overlap_matrix(Minus_2_Resolvent_Phi, Phi_n);
+        //std::cout << "B_proj matrix:" << std::endl << B_proj.real() << std::endl;
+        ComplexMatrix C_proj_complex = orbital::calc_overlap_matrix(grad_E, Phi_n);
+        DoubleMatrix C_proj_sym = (C_proj_complex.real() + C_proj_complex.real().transpose()) * 0.5;
+        DoubleMatrix B_proj_real = (B_proj.real() + B_proj.real().transpose()) * 0.5;
 
+        DoubleMatrix A_proj = mrchem::math_utils::solve_symmetric_sylvester(B_proj_real, C_proj_sym);
+        DoubleMatrix minus_half_A_proj = -0.5 * A_proj;
+        //std::cout << "A_proj matrix:" << std::endl << A_proj << std::endl;
 
-        OrbitalVector Phi_np1 = grad_E;
+        OrbitalVector AR_Phi = orbital::rotate(Minus_2_Resolvent_Phi, minus_half_A_proj);
+        grad_E = orbital::add(1.0, grad_E, -1.0, AR_Phi);
+        auto grad_E_norm = orbital::get_norms(grad_E).maxCoeff();
+        std::cout << "norm(grad_E) = " << grad_E_norm << std::endl;
+        std::cout << "--------------------------------------" << std::endl;
+
         F.clear();
 
         // Orthonormalize
