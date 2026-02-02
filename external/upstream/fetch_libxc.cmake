cdd0549cf55900c1b4d7ac40541d58b35ba0a897
ylvao <ylvaos1@gmail.com>
2026-02-02 15:24:40 +0100

    makes libxc dir in install/include

--- external/upstream/fetch_libxc.cmake
+++ external/upstream/fetch_libxc.cmake
@@ -1,14 +1,16 @@
 find_package(Libxc QUIET CONFIG)
 
 if(TARGET Libxc::xc)
-  get_property(_loc TARGET Libxc::xc PROPERTY LOCATION)
+  get_target_property(_loc Libxc::xc LOCATION)
   message(STATUS "Libxc::xc: ${_loc} (found version ${Libxc_VERSION})")
 else()
   message(STATUS "Suitable LibXC could not be located. Fetching and building!")
-
   include(FetchContent)
-
-  # Set LibXC options BEFORE bringing it in
+  FetchContent_Declare(libxc_sources
+    GIT_REPOSITORY https://gitlab.com/libxc/libxc
+    GIT_TAG 7.0.0
+  )
+  set(CMAKE_INSTALL_INCLUDEDIR "include/" CACHE STRING "" FORCE) # Creates Libxc subdir in install
   set(BUILD_TESTING     OFF CACHE BOOL "Build LibXC tests" FORCE)
   set(ENABLE_TESTS      OFF CACHE BOOL "Enable LibXC tests" FORCE)
   set(BUILD_SHARED_LIBS ON  CACHE BOOL "Build LibXC shared libs" FORCE)
@@ -17,27 +19,18 @@ else()
   set(DISABLE_KXC       ON  CACHE BOOL "Disable 3rd derivatives (Kxc)" FORCE)
   set(DISABLE_LXC       ON  CACHE BOOL "Disable 4th derivatives (Lxc)" FORCE)
 
-  FetchContent_Declare(libxc_sources
-    GIT_REPOSITORY https://gitlab.com/libxc/libxc
-    GIT_TAG 7.0.0
-  )
-
   FetchContent_MakeAvailable(libxc_sources)
 
-  # If libxc builds target "xc", create the imported-style alias your project expects
   if(TARGET xc)
-  # Create alias if not already provided
-  if(NOT TARGET Libxc::xc)
-    add_library(Libxc::xc ALIAS xc)
-  endif()
+    if(NOT TARGET Libxc::xc)
+      add_library(Libxc::xc ALIAS xc)
+    endif()
 
-  # Ensure headers are reachable when using the build tree
-  # (If LibXC does not fully publish build-tree include dirs)
-  target_include_directories(xc
-    INTERFACE
-      $<BUILD_INTERFACE:${libxc_sources_SOURCE_DIR}/src>
-      $<BUILD_INTERFACE:${libxc_sources_BINARY_DIR}>
-  )
+    target_include_directories(xc
+      INTERFACE
+        $<BUILD_INTERFACE:${libxc_sources_SOURCE_DIR}/src>
+        $<BUILD_INTERFACE:${libxc_sources_BINARY_DIR}>
+        $<INSTALL_INTERFACE:include/Libxc> 
+    )
   endif()
-
 endif()
