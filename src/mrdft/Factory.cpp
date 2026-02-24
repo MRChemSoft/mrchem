bf7540fa0c8a10e3e31a7155eb4c778e3c884b81
ylvao <ylvaos1@gmail.com>
2026-02-24 09:26:47 +0100

    mapping more functionals

--- src/mrdft/Factory.cpp
+++ src/mrdft/Factory.cpp
@@ -42,44 +42,70 @@ Factory::Factory(const mrcpp::MultiResolutionAnalysis<3> &MRA)
         : mra(MRA)
         , xcfun_p(xcfun_new(), xcfun_delete) {}
 
-bool Factory::libxc;
-
 void MapFuncName(std::string name, std::vector<int> &ids, std::vector<double> &coefs) {
     // ensure name is upper case
     std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return std::toupper(c); });
 
-    if (name == "PBE0") {
-        ids = {XC_HYB_GGA_XC_PBEH};
-        coefs = {1.0};
-        return;
-    } else if (name == "PBE") {
-        ids = {XC_GGA_X_PBE, XC_GGA_C_PBE};
-        coefs = {1.0, 1.0};
-        return;
-    } else if (name == "SLATERX") {
+    // functionals defined in mrchem not explicitly translated to libxc:
+    // b3lyp-g, b3p86-g, olyp
+
+    // LDA
+    if (name == "SLATERX") {
         ids = {XC_LDA_X};
         coefs = {1.0};
         return;
+    } else if (name == "SVWN3C" || name == "VWN3C") {
+        ids = {XC_LDA_C_VWN};
+        coefs = {1.0};
+        return;
+    } else if (name == "SVWN5C" || name == "VWN5C") {
+        ids = {XC_LDA_C_VWN};
+        coefs = {1.0};
+        return;
+    } else if (name == "SVWN3") {
+        ids = {XC_LDA_C_VWN, XC_LDA_X};
+        coefs = {1.0, 1.0};
+        return;
+    } else if (name == "SVWN5" || name == "LDA") {
+        ids = {XC_LDA_C_VWN_3, XC_LDA_X};
+        coefs = {1.0, 1.0};
+        return;
+
+    // GGA
     } else if (name == "BECKEX") {
         ids = {XC_GGA_X_B88};
         coefs = {1.0};
         return;
-    } else if (name == "VWN5C") {
-        ids = {XC_LDA_C_VWN};
-        coefs = {1.0};
-        return;
-    } else if (name == "SVWN5") {
-        ids = {XC_LDA_C_VWN, XC_LDA_X};
+    } else if (name == "BLYP") {
+        ids = {XC_GGA_X_B88, XC_GGA_C_LYP};
         coefs = {1.0, 1.0};
         return;
-    } else if (name == "B3P86") {
-        ids = {XC_HYB_GGA_XC_B3P86};
-        coefs = {1.0};
+    } else if (name == "BP86") {
+        ids = {XC_GGA_X_B88, XC_GGA_C_P86};
+        coefs = {1.0, 1.0};
         return;
     } else if (name == "BPW91") {
         ids = {XC_GGA_X_B88, XC_GGA_C_PW91};
         coefs = {1.0, 1.0};
+    } else if (name == "PBE") {
+        // NB: not the exact same parameters, eq to 1e-7 for H2
+        ids = {XC_GGA_X_PBE, XC_GGA_C_PBE};
+        coefs = {1.0, 1.0};
         return;
+    } else if (name == "KT1") {
+        ids = {XC_GGA_XC_KT1};
+        coefs = {1.0};
+        return;
+    } else if (name == "KT2") {
+        ids = {XC_GGA_XC_KT2};
+        coefs = {1.0};
+        return;
+    } else if (name == "KT3") {
+        ids = {XC_GGA_XC_KT3};
+        coefs = {1.0};
+        return;
+
+    // HYB GGA
     } else if (name == "B3LYP") {
         // Keep as b3lyp5 for now to be consistent with xcfun
         // TODO: change the definition of b3lyp in mrchem to not be b3lyp5
@@ -89,6 +115,16 @@ void MapFuncName(std::string name, std::vector<int> &ids, std::vector<double> &c
         return;
     } else if (name == "B3LYP5") {
         ids = {XC_HYB_GGA_XC_B3LYP5};
+        coefs = {1.0};
+        return;
+    } else if (name == "B3P86") {
+        // NB: not the exact same parameters, eq to 1e-3 for H2
+        ids = {XC_HYB_GGA_XC_B3P86};
+        coefs = {1.0};
+        return;
+    } else if (name == "PBE0") {
+        // NB: not the exact same parameters, eq to 1e-7 for H2
+        ids = {XC_HYB_GGA_XC_PBEH};
         coefs = {1.0};
         return;
     } else {
