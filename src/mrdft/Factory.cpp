cad4fe994577c867dd9166567294e03153b14be4
Ylva Os <75043847+ylvao@users.noreply.github.com>
2026-02-24 14:42:26 +0100

    removing svwnxc name option

--- src/mrdft/Factory.cpp
+++ src/mrdft/Factory.cpp
@@ -56,11 +56,11 @@ void MapFuncName(std::string name, std::vector<int> &ids, std::vector<double> &c
         ids = {XC_LDA_X};
         coefs = {1.0};
         return;
-    } else if (name == "SVWN3C" || name == "VWN3C") {
+    } else if (name == "VWN3C") {
         ids = {XC_LDA_C_VWN_3};
         coefs = {1.0};
         return;
-    } else if (name == "SVWN5C" || name == "VWN5C") {
+    } else if (name == "VWN5C") {
         ids = {XC_LDA_C_VWN};
         coefs = {1.0};
         return;
